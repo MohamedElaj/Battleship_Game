d4ab7d1fec371625dcdad281a6701cf1806d6e07
Mohamed El Ajouaoui <Mohamedelajouaoui99@gmail.com>
2024-05-17 10:50:37 +0200

    SimpleStartupTestDrive überarbeitet

--- src/SimpleStartupTestDrive.java
+++ src/SimpleStartupTestDrive.java
@@ -6,7 +6,10 @@ public class SimpleStartupTestDrive {
         GameHelper helper = new GameHelper();
         SimpleStartup theStartup = new SimpleStartup();
 
+        // Lassen nur eine Zahl generieren, dass dann randomNum zugewiesen wird.
         int randomNum = (int) (Math.random() * 5);
+        // Diese eine Zahl ist der Ort auf einer Zeile. Weil drei Position eingenommen
+        // werden müssen (Spielregeln) addieren wir die Zahl für jede Stelle mit 1 und 2.
         int [] locations = {randomNum, randomNum + 1, randomNum + 2};
 
         theStartup.setLocationCells(locations);
