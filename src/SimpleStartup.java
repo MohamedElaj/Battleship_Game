1c393f27450e148b72127b018818cc8914420b55
Mohamed El Ajouaoui <Mohamedelajouaoui99@gmail.com>
2024-05-17 09:49:01 +0200

    Funktion checkYourself hinzugefügt

--- src/SimpleStartup.java
+++ src/SimpleStartup.java
@@ -1,2 +1,21 @@
 public class SimpleStartup {
+
+    public String checkYourself(int guess) {
+        String result = "miss";
+
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }
+
+        if (numOfHits == locationCells.length) {
+            result = "kill";
+        }
+
+        System.out.println(result);
+        return result;
+    }
 }
